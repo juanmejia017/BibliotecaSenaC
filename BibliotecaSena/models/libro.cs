c96ec8a6cd12474a1de3b6f05b3b392f02467179
juanmejia017 <juanmejia0712123@gmail.com>
2026-03-22 14:05:05 -0500

    feat:add properties to libro class

--- BibliotecaSena/models/libro.cs
+++ BibliotecaSena/models/libro.cs
@@ -4,5 +4,12 @@ namespace Models
 {
     public class Libro
     {
+      public int Id { get; set; }
+public string Titulo { get; set; }
+public string Autor { get; set; }
+public int Anio { get; set; }
+public string Categoria { get; set; }
+public bool Disponible { get; set; } = true;  
+        
     }
 }
